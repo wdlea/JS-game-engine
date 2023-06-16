5e81eeef85adf0361a985c669ab11a6e4d79adbc
wdlea <wdleader18@gmail.com>
2023-06-16 21:24:38 +1200

    Added altered positon

--- ts/cursor/cursor.ts
+++ ts/cursor/cursor.ts
@@ -1,10 +1,11 @@
-import { vec2 } from "gl-matrix";
+import { vec2, vec4 } from "gl-matrix";
 import { Camera } from "../core";
-import { Ray } from "../math";
+import { Ray, Remap } from "../math";
 
 class Cursor {
     private camera: Camera;
     private ray: Ray | undefined;
+    private cursorElement: HTMLElement | undefined;
 
     get Ray(): Ray {
         if (this.ray === undefined) {
@@ -16,6 +17,20 @@ class Cursor {
     pos: vec2;
     get Pos(): vec2 {
         return this.pos
+    }
+
+    get AlteredPos(): vec2 {
+        const point = this.Ray.Step(1)
+        const newPos = vec4.transformMat4(point, point, this.camera.CameraMatrix.ProjectionMatrix)
+
+        if (this.camera._gl.canvas instanceof OffscreenCanvas)
+            throw Error("Canvas is offscreen")
+
+        const canvasPos = vec2.fromValues(
+            Remap(newPos[0], -1, 1, 0, this.camera._gl.canvas.clientWidth),
+            Remap(newPos[1], -1, 1, 0, this.camera._gl.canvas.clientHeight),
+        )
+        return canvasPos
     }
 
     constructor(camera: Camera) {
